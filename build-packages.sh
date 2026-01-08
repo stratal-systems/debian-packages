9300beb2572b4725e70f7efb2b30917141e68f56
maybetree <maybetree48@proton.me>
2026-01-08 10:44:17 +0100

    add libgsl

--- build-packages.sh
+++ build-packages.sh
@@ -1,9 +1,37 @@
 #!/bin/sh
 
-set -e
+pkgs="libhdf5-static-pic libgsl-static-pic"
+
+# echo to stderr
+eecho() {
+	echo "$@" >&2
+}
+
+# echo to stderr and exit with error
+die() {
+	eecho "$@"
+	exit 1
+}
+
+cd_ass() {
+	cd "$1" || die "Failed to cd into ${2:-$1}"
+}
 
 export DH_VERBOSE=1 
 
-cd pkg/libhdf5-static-pic
-dpkg-buildpackage -b
+for pkg in $pkgs
+do
+	eecho "Processing $pkg"
+	if ls ./pkg/"${pkg}"*.deb
+	then
+		eecho "exists, skipping."
+		continue
+	fi
+
+	cd_ass "./pkg/${pkg}"
+	dpkg-buildpackage -b || die "Failed to build $pkg"
+	cd_ass ..
+done
+
+eecho "Done!"
 
